9399dbc6b185b43ba18e411836c71297d1476eec
Matt Sephton <matt.sephton@gmail.com>
2012-07-19 12:06:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# GeoIPFlags
+
+Adds a column in Dashboard/Users that has the GeoIP country flag of the last IP Address of the user.
+
+Modification of the existing plugin to fix several bugs:  
+
+- Fixes paths for case sensitive filesystems.  
+- Empty flag image for users with no Last IP  
+- Table header only output for users with no name (now skips Akismet plugin)  
+- Uses GEOIP_MEMORY_CACHE for speed  
+- Closes filehandle after geoip_open
+- PNG images have been optimised
+
+## Changelog
+1.1, modification of the existing plugin to fix several bugs  
+1.0, initial release by vbgamer45  
+
+## Details
+See [http://vanillaforums.org/addon/easyreply-plugin](http://vanillaforums.org/addon/easyreply-plugin)
+
+## Installation
+Place the unzipped plugin folder in your `vanilla/plugins` folder and then activate using the Dashboard.  
+
+### License
+This plugin is made available under a [Creative Commons Attribution-Share Alike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0).
+
+### Credits
+Created by [vbgamer45](http://vanillaforums.org/profile/37130/vbgamer45), modified by Matt Sephton, [http://www.gingerbeardman.com/vanilla/](http://www.gingerbeardman.com/vanilla/)
---
+++ default.php
@@ -0,0 +1,52 @@
+<?php 
+/*
+GeoIP Country Flags for Vanilla Forums
+By: vbgamer45, johansonlocker, gingerbeardman
+http://wwww.vanillaforfree.com
+
+*/
+if (!defined('APPLICATION')) 
+	exit();
+
+$PluginInfo['GeoIPFlags'] = array(
+	'Name' => 'GeoIPFlags',
+	'Description' => 'Adds a GeoIP Country Flag column to the users list in the admin panel',
+	'Version' => '1.1',
+	'Date' => 'July 19, 2012',
+	'Author' => 'vbgamer45, johansonlocker, gingerbeardman',
+	'AuthorEmail' => 'vbgamer45@gmail.com',
+	'AuthorUrl' => 'http://www.vanillaforfree.com',
+	'RequiredApplications' => array(
+		'Dashboard' => '>=2.1'
+	)
+);
+
+
+class GeoIPFlags implements Gdn_IPlugin 
+{
+	
+
+	public function UserController_UserCell_Handler($Sender) 
+	{
+		if (empty($Sender->EventArgs['User']->Name))
+			echo '<th>GeoIP Country</th>';
+		else 
+		{
+			include_once(dirname(__FILE__) . "/extras/geoip.php");
+			$gi = geoip_open(dirname(__FILE__) . "/extras/GeoIP.dat", GEOIP_MEMORY_CACHE);
+
+			$countrycode = geoip_country_code_by_addr($gi, $Sender->EventArgs['User']->LastIPAddress);
+			if (!$countrycode) $countrycode = 'none';
+			geoip_close($gi);
+				
+			echo '<td><img src="/plugins/GeoIPFlags/flags/' . strtolower($countrycode) . '.png" alt="' . $countrycode . '" /></td>';
+		}
+		
+	}
+
+	
+	public function Setup() 
+	{
+		
+	}
+}
