0f5a844bb3e5c10d0e7acf7bee6900f51312c1a0
Matt Sephton <matt.sephton@gmail.com>
2012-07-19 17:00:15 +0100

    adds flag to user profile

--- class.worldflags.plugin.php
+++ class.worldflags.plugin.php
@@ -5,8 +5,7 @@ if (!defined('APPLICATION'))
 $PluginInfo['WorldFlags'] = array(
 	'Name' => 'World Flags',
 	'Description' => 'Adds a column in Dashboard/Users with a country flag displaying the location of the Last IP Address for each user.',
-	'Version' => '1.0',
-	'Date' => 'July 19, 2012',
+	'Version' => '1.1',
 	'Author' 	=>	 "Matt Sephton",
 	'AuthorEmail' => 'matt@gingerbeardman.com',
 	'AuthorUrl' =>	 'http://www.gingerbeardman.com',
@@ -23,44 +22,63 @@ class WorldFlags implements Gdn_IPlugin
 	public function Base_Render_Before($Sender) {
 		$this->_WorldFlagsSetup($Sender);
 	}
-	
+
 	private function _WorldFlagsSetup($Sender) {
 		$Sender->AddCssFile('flags.css', 'plugins/WorldFlags');
 	}
 	
-	public function UserController_UserCell_Handler($Sender) 
-	{
-		if (empty($Sender->EventArgs['User']->Name))
+	private function _GeoLiteCity($Sender, $UseLocation) {
+		$extraspath = dirname(__FILE__).'/extras/';
+
+		if ($UseLocation == 'DashboardUsers') $ip = $Sender->EventArgs['User']->LastIPAddress;
+		if ($UseLocation == 'UserProfile') $ip = $Sender->User->LastIPAddress;
+
+		include_once($extraspath."geoipcity.inc");
+		include_once($extraspath."geoipregionvars.php"); 
+
+		$gi = geoip_open($extraspath."GeoLiteCity.dat", GEOIP_MEMORY_CACHE);
+		$record = geoip_record_by_addr($gi, $ip);
+		geoip_close($gi);
+
+		$countrycode = strtolower($record->country_code);
+		if (!$countrycode) {
+			$countrycode = 'none';
+			$title = '';
+		} else {
+			$countryname = $record->country_name;
+			$cityname = utf8_decode($record->city);
+			$title =  "$countryname, $cityname";
+		}
+		
+		return array($countrycode, $title);
+	}
+	
+	public function UserInfoModule_OnBasicInfo_Handler($Sender) {
+		list($countrycode, $title) = $this->_GeoLiteCity($Sender, 'UserProfile');
+		
+		echo "<dt>Location</dt>";
+		echo <<< USER
+				<dd>
+					<div class="flag flag-$countrycode" title="$title">
+						<span>$title</span>
+					</div>
+				</dd>
+USER;
+	}
+	
+	public function UserController_UserCell_Handler($Sender) {
+		if (empty($Sender->EventArgs['User']->Name)) {
 			echo '<th>Location</th>';
-		else 
-		{
-			$extraspath = dirname(__FILE__).'/extras/';
-			$ip = $Sender->EventArgs['User']->LastIPAddress;
+		} else {
+			list($countrycode, $title) = $this->_GeoLiteCity($Sender, 'DashboardUsers');
 
-			include_once($extraspath."geoipcity.inc");
-			include_once($extraspath."geoipregionvars.php"); 
-
-			$gi = geoip_open($extraspath."GeoLiteCity.dat", GEOIP_MEMORY_CACHE);
-			$record = geoip_record_by_addr($gi, $ip);
-			geoip_close($gi);
-
-			$countrycode = strtolower($record->country_code);
-			if (!$countrycode) {
-				$countrycode = 'none';
-				$title = '';
-			} else {
-				$countryname = $record->country_name;
-				$cityname = utf8_decode($record->city);
-				$title =  "$countryname, $cityname";
-			}
-
-			echo <<< FLAG
+			echo <<< DASH
 				<td>
 					<div class="flag flag-$countrycode" title="$title">
 						<span>$title</span>
 					</div>
 				</td>
-FLAG;
+DASH;
 		}
 		
 	}
